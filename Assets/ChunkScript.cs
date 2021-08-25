99043af71069c4b7b00de0198c00e68f58db56e0
carector <baseline7@live.com>
2021-08-25 03:15:07 -0700

    Got endless mode level generation working

---
+++ Assets/ChunkScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkScript : MonoBehaviour
+{
+    public Transform[] entrances; // 0 = N, 1 = S, 2 = E, 3 = W
+    public bool[] occupiedEntrances;
+
+    LevelChunkManager lcm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lcm = FindObjectOfType<LevelChunkManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InitializeChunkPosition(Vector3 entrancePosition, int entranceDirection)
+    {
+        // Position should be set to opposite of given entrance direction (e.g. if provided north, we set to south)
+        Vector3 offset = Vector3.zero;
+        switch(entranceDirection)
+        {
+            case 0:
+                occupiedEntrances[1] = true;
+                offset = entrances[1].position - transform.position;
+                break;
+            case 1:
+                occupiedEntrances[0] = true;
+                offset = entrances[0].position - transform.position;
+                break;
+            case 2:
+                occupiedEntrances[3] = true;
+                offset = entrances[3].position - transform.position;
+                break;
+            case 3:
+                occupiedEntrances[2] = true;
+                offset = entrances[2].position - transform.position;
+                break;
+        }
+
+        transform.position = entrancePosition - offset;
+    }
+}
---
+++ Assets/LevelChunkManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChunkManager : MonoBehaviour
+{
+    public ChunkScript activeChunk;
+    public ChunkScript previousChunk;
+    public ChunkScript[] chunkPool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ChunkPlacer());
+    }
+
+    IEnumerator ChunkPlacer()
+    {
+        PlaceNeighboringChunk();
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(ChunkPlacer());
+    }
+
+    public void PlaceNeighboringChunk()
+    {
+        // Get random compass direction to place chunk at
+        // Make sure we don't place a chunk where it's already connected to one
+        int dir = -1;
+        while(dir == -1)
+        {
+            int rand = Random.Range(0, 4);
+            if (!activeChunk.occupiedEntrances[rand])
+                dir = rand;
+        }
+
+        // Get next chunk object, avoid spawning duplicate chunks
+        ChunkScript c = chunkPool[Random.Range(0, chunkPool.Length)];
+        while(c == activeChunk)
+            c = chunkPool[Random.Range(0, chunkPool.Length)];
+
+        c = Instantiate(c).GetComponent<ChunkScript>();
+
+        print(dir);
+        c.InitializeChunkPosition(activeChunk.entrances[dir].position, dir);
+
+        if(previousChunk != null)
+            Destroy(previousChunk.gameObject);
+        previousChunk = activeChunk;
+        activeChunk = c;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
